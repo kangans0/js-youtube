94f406a63308eef49c8d06f36255752c26b702ca
Kangan Sharma <skangan78@gmail.com>
2025-06-08 20:56:35 +0530

    learning datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello") // we are using node.js, not browser
+let name="kangan"
+let age=18
+let isLoggedIn= false
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+//  boolean
