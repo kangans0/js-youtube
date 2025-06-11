fd3d69b64f4c502ff2e09b2693c9d63139c375d5
Kangan Sharma <skangan78@gmail.com>
2025-06-11 22:43:50 +0530

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+// let score ="33abc"
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn =""
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+//  "" => false
+//  "kangan" => true
+
+// let someNumber =33
+// let stringNumber =String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Operations ********************
+
+// let value =3
+// let negvalue=-value
+// console.log(negvalue)
+
+// let str1= "hello"
+// let str2=" kangan"
+// let str3= str1+str2
+// console.log(str3)
+// Complex situation
+console.log("1"+2);
+console.log("1"+2+2);  
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
