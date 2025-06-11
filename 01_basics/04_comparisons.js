5dce7b34e0963102faadf3d56576fd3a654e9ead
Kangan Sharma <skangan78@gmail.com>
2025-06-11 22:58:59 +0530

    comparing datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+console.log("2">1);   //always compare same datatype
+// console.log("02">1);  // WRONG PRACTISE DONT DO THIS EVER
+
+console.log(null>0)
+console.log(null==0)  //here it is converting null to 0
+console.log(null>=0)  // so here it is giving true //value conversion problem
+
+//  === "string checking"
+console.log("2" === 2);
