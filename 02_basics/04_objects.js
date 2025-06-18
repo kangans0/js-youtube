c9ed81404ddde4290ed48238118dcab2886d1c40
Kangan Sharma <skangan78@gmail.com>
2025-06-18 21:16:24 +0530

    Object3

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,7 +26,7 @@ const obj2= {3: "a", 4: "b"}
 // {} this act as a target and rest other act as a source
 
 const obj3 ={...obj1,...obj2} //this will be used 90% of time same as array
-console.log(obj3);
+// console.log(obj3);
 
 const users =[
 {
@@ -43,12 +43,30 @@ const users =[
 ]
 
 // users[1].email
-console.log(tinderUser)
-// important
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-// to check if it include any this named property
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser)
+// // important
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// // to check if it include any this named property
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
 
 // To check more properties go on console make a object and check
+
+// *********************OBJECT de-structure and JSON API*******************
+// JSON full form is JavaScript Object Notation
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor OR use const {courseInstructor}= course
+//  const {courseInstructor: instructor}= course
+// console.log(instructor)
+
+// {
+//     "name": "Kangan",
+//     "coursename": "js in hindi",
+//     "price" ="free"
+// }
