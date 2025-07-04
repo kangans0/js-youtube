06afd29317841b445bf3eb1e0a37558b167bf712
Kangan Sharma <skangan78@gmail.com>
2025-07-04 15:03:06 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -98,11 +98,11 @@
 // getAllUsers()
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response)=>{
-    return response.json()
-})
-.then((data)=>{
-    console.log(data)
-})
-.catch((error)=> console.log(error))
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data)
+// })
+// .catch((error)=> console.log(error))
